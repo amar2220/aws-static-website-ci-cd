28bf6703a7665ab3b8b5ef9efd9e4fafda745077
amar2220 <167752418+amar2220@users.noreply.github.com>
2026-03-27 20:21:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function showMessage() {
-    alert("Welcome to my DevOps Project ");
-}
+// SMOOTH SCROLL (if you add nav links later)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+
+// SCROLL ANIMATION
+const cards = document.querySelectorAll('.card');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+cards.forEach(card => {
+    observer.observe(card);
+});
